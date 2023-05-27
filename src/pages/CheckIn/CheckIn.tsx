b064f55237c33666994705f200604172fb853dbf
Yaron Veg <Yaronveg1@gmail.com>
2023-05-27 17:24:48 +0300

    update the check in component

--- src/pages/CheckIn/CheckIn.tsx
+++ src/pages/CheckIn/CheckIn.tsx
@@ -1,24 +1,33 @@
-function CheckIn() {
-    return (
-      <div className="check-in">
-        check-in page
-        <div className="floating-logos">
-          <div className="logo-left"></div>
-          <div className="logo-right"></div>
-        </div>
-        <div className="title">
-          <div className="title-introducing"></div>
-          <div className="title-main"></div>
-        </div>
-        <div className="numpad-wrapper">
-          <div className="id-info"></div>
-          <div className="id-display"></div>
-          <div className="numpad"></div>
-          <div className="submit-button"></div>
-        </div>
+import { useState } from "react"
+import Numpad, { NumpadButton } from "./components/Numpad"
+
+const CheckIn = () => {
+const [idNum, setIdNum] = useState<string>("")
+
+const numpadChangeHandler = ({button}:{button: NumpadButton}) => {
+  setIdNum(button)
+  console.log("checkin: ", { button })
+}
+
+  return (
+    <div className="check-in">
+      check-in page
+      <div className="floating-logos">
+        <div className="logo-left"></div>
+        <div className="logo-right"></div>
       </div>
-    );
-  }
-  
-  export default CheckIn;
-  
+      <div className="title">
+        <div className="title-introducing"></div>
+        <div className="title-main"></div>
+      </div>
+      <div className="numpad-wrapper">
+        <div className="id-info"></div>
+        <div className="id-display">{idNum}</div>
+        <Numpad onChange={numpadChangeHandler} />
+        <div className="submit-button"></div>
+      </div>
+    </div>
+  )
+}
+
+export default CheckIn
