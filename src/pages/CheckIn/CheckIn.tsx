6786ad4eac5b9c02e2b17411d01b3a8d6b4a17e6
Yaron Veg <Yaronveg1@gmail.com>
2023-05-27 18:04:13 +0300

    numpad working

--- src/pages/CheckIn/CheckIn.tsx
+++ src/pages/CheckIn/CheckIn.tsx
@@ -1,12 +1,21 @@
 import { useState } from "react"
 import Numpad, { NumpadButton } from "./components/Numpad"
+import "./CheckIn.scss"
+import IdDisplay from "./components/IdDisplay"
 
 const CheckIn = () => {
 const [idNum, setIdNum] = useState<string>("")
 
-const numpadChangeHandler = ({button}:{button: NumpadButton}) => {
-  setIdNum(button)
-  console.log("checkin: ", { button })
+const numpadChangeHandler = ({button}: {button: NumpadButton}) => {
+      if (button === "CLEAR") {
+        setIdNum("");
+        return;
+      }
+      if (button === "BACKSPACE") {
+        setIdNum(idNum.slice(0, idNum.length - 1));
+        return;
+      }
+      if (idNum.length < 9) setIdNum(idNum + button);
 }
 
   return (
@@ -16,13 +25,15 @@ const numpadChangeHandler = ({button}:{button: NumpadButton}) => {
         <div className="logo-left"></div>
         <div className="logo-right"></div>
       </div>
-      <div className="title">
+      <div className="title container">
         <div className="title-introducing"></div>
         <div className="title-main"></div>
       </div>
-      <div className="numpad-wrapper">
+      <div className="numpad-wrapper container">
         <div className="id-info"></div>
-        <div className="id-display">{idNum}</div>
+        <div className="id-display-wrapper">
+          <IdDisplay id={idNum} />
+        </div>
         <Numpad onChange={numpadChangeHandler} />
         <div className="submit-button"></div>
       </div>
---
+++ src/pages/CheckIn/components/IdDisplay.tsx
@@ -0,0 +1,11 @@
+import "./IdDisplay.scss"
+
+const IdDisplay = ({id}: {id:string}) => {
+  return (
+    <div className="id-display">
+      {id}
+    </div>
+  )
+}
+
+export default IdDisplay
