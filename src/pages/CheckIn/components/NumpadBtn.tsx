e44709188bdc2d9d43d99b440edb30b802f37b26
Yaron Veg <Yaronveg1@gmail.com>
2023-05-27 17:25:17 +0300

    add numpad button

---
+++ src/pages/CheckIn/components/NumpadBtn.tsx
@@ -0,0 +1,12 @@
+import "./NumpadBtn.scss"
+import { NumpadButton } from "./Numpad"
+
+const NumpadBtn = ({button, onClick}: {button: NumpadButton, onClick: ( (button: NumpadButton) => void )}) => {
+  return (
+    <button className="numpad-btn" onClick={() =>onClick(button)}>
+      {button}
+    </button>
+  )
+}
+
+export default NumpadBtn
