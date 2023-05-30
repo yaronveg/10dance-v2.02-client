f2e0c61a55b320c6b6e4d6e52f6201ca578a2c88
Yaron Veg <Yaronveg1@gmail.com>
2023-05-30 23:10:06 +0300

    set numpad icons

--- src/pages/CheckIn/components/NumpadBtn.tsx
+++ src/pages/CheckIn/components/NumpadBtn.tsx
@@ -1,5 +1,8 @@
 import "./NumpadBtn.scss";
 import { NumpadButton } from "./Numpad";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 const NumpadBtn = ({
   button,
@@ -8,9 +11,19 @@ const NumpadBtn = ({
   button: NumpadButton;
   onClick: (button: NumpadButton) => void;
 }) => {
+  const [buttonLabel, setButtonLabel] = useState<NumpadButton | JSX.Element>(
+    button
+  );
+
+  useEffect(() => {
+    if (button === "CLEAR") setButtonLabel(<FontAwesomeIcon icon={faTrash} />);
+    if (button === "BACKSPACE")
+      setButtonLabel(<FontAwesomeIcon icon={faChevronLeft} />);
+  }, []);
+
   return (
     <button className="numpad-btn" onClick={() => onClick(button)}>
-      {button}
+      {buttonLabel}
     </button>
   );
 };
