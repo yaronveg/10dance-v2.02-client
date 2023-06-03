aae8ba5780f5871c9a827a45e5d40418725751d3
Yaron Veg <Yaronveg1@gmail.com>
2023-06-03 17:54:09 +0300

    rename attendee check in

--- src/pages/CheckIn/api/check-in.ts
+++ src/pages/CheckIn/api/check-in.ts
@@ -1,6 +1,6 @@
 const base_url = import.meta.env.VITE_BASE_URL;
 
-export const checkInGuest = async (id: string): Promise<Response> => {
+export const checkInAttendee = async (id: string): Promise<Response> => {
         const url = `${base_url}/attendee/check_in/${id}`;
         const res = await fetch(url, { method: "POST" });
         return res;
