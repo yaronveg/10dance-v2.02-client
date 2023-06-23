42b378abc83ffc47da0a53e2626422d0e66441d7
Yaron Veg <Yaronveg1@gmail.com>
2023-06-23 17:43:24 +0300

    use modal in delete row and remove test modal

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -6,13 +6,11 @@ import Table from "./components/Table/Table";
 import { useState, useCallback, useEffect } from "react";
 import { getAllAttendees } from "../../common/api/attendee";
 import Fuse from "fuse.js";
-import Modal from "./components/Modal";
 
 function Admin() {
   const [allAttendees, setAllAttendees] = useState<Attendee[]>([]);
   const [visibleRows, setVisibleRows] = useState<Attendee[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [showModal, setShowModal] = useState(true);
 
   const getRows = useCallback(async () => {
     const resJson = await getAllAttendees();
@@ -53,14 +51,6 @@ function Admin() {
   return (
     <div className="admin-wrapper">
       <div className="flex-none">
-        {showModal && (
-          <Modal
-            onClose={() => setShowModal(false)}
-            header={<span>כותרת</span>}
-            content={<div>תוכן ראשי</div>}
-            submitLabel="מחיקה"
-          />
-        )}
         <Header />
       </div>
       <div className="flex-1 main width-container">
--- src/pages/Admin/components/Table/TableRow.tsx
+++ src/pages/Admin/components/Table/TableRow.tsx
@@ -6,17 +6,23 @@ import {
   faPrint,
   faTrash,
 } from "@fortawesome/free-solid-svg-icons";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { deleteAttendeeById } from "../../../../common/api/attendee";
 import { useNavigate } from "react-router-dom";
+import Modal from "../Modal";
 
 const TableRow = ({ row }: { row: Attendee }) => {
   const navigate = useNavigate();
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
   const deleteAttendee = useCallback(() => {
     deleteAttendeeById({ id: row.national_id });
   }, []);
 
+  const modalDeleteHandler = useCallback(() => {
+    deleteAttendee();
+    setShowDeleteModal(false);
+  }, []);
   const printAttendeeTag = () => {
     navigate("/print", {
       state: {
@@ -38,8 +44,28 @@ const TableRow = ({ row }: { row: Attendee }) => {
       </div>
       <div className="actions">
         <FontAwesomeIcon icon={faPrint} onClick={printAttendeeTag} />
-        <FontAwesomeIcon icon={faTrash} onClick={deleteAttendee} />
+        <FontAwesomeIcon
+          icon={faTrash}
+          onClick={() => setShowDeleteModal(true)}
+        />
       </div>
+
+      {showDeleteModal && (
+        <Modal
+          onClose={() => setShowDeleteModal(false)}
+          onSubmit={modalDeleteHandler}
+          header={<div>מחיקת שורה</div>}
+          content={
+            <div>
+              <div>
+                האם למחוק את "{row.first_name} {row.last_name}"?
+              </div>
+              <b>פעולה זו אינה ניתנת לביטול.</b>
+            </div>
+          }
+          submitLabel="מחיקה"
+        />
+      )}
     </div>
   );
 };
